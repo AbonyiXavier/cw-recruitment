efa8a04206e30f3174d79feffc940c760997e9ec
AbonyiXavier <francisabonyi@gmail.com>
2023-10-27 20:37:36 +0200

    APIs implementation and swagger configuration

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -1,25 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { ClientModule } from '../client/client.module';
 import { AwsS3UploadModule } from '../aws-s3-upload/aws-s3-upload.module';
 
 @Module({
-  imports: [
-    ClientModule,
-    AwsS3UploadModule,
-    // JwtModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (configService: ConfigService) => ({
-    //     secret: configService.get<string>('JWT_SECRET'),
-    //     signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
-    //   }),
-    //   inject: [ConfigService],
-    // }),
-  ],
+  imports: [ClientModule, AwsS3UploadModule],
   controllers: [AuthController],
   providers: [AuthService, JwtService, JwtStrategy],
   exports: [AuthService],
